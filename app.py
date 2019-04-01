05742e426210c75ff73ed9422f768c139ecc6972
Eric Colburn <ericcolburn@yahoo.com>
2019-04-01 18:59:15 -0400

    adding webscraping and Flask app

---
+++ app.py
@@ -0,0 +1,72 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+from config import remote_db_endpoint, remote_db_port
+from config import remote_gwsis_dbname, remote_gwsis_dbuser, remote_gwsis_dbpwd
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Password1!!!@codingbootcamp.ctxjv3tnsa2p.us-east-2.rds.amazonaws.com/marchmadness"
+db = SQLAlchemy(app)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+
+data_base_table = Base.classes.earnings
+
+
+@app.route("/")
+def samples():
+   
+    stmt = db.session.query(data_base_table).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    data = df.to_dict()
+
+    return (
+    jsonify(data)
+    )
+
+@app.route("/list")
+def list_data():
+    stmt = db.session.query(data_base_table).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    data_list = {
+        "Index": df.level_0.values.tolist(),
+        "Stock": df['Stock Name'].values.tolist(),
+        "Reported Date": df['Reported Date'].values.tolist(),
+        "Earnings Per Share": df['Earnings Per Share'].values.tolist(),
+        "Forecasted Earnings Per Share": df['Forecasted Earnings Per Share'].values.tolist(),
+        "% Surprise": df['% Surprise'].values.tolist()
+    }
+    return (
+    jsonify(data_list)
+    )
+
+@app.route("/dict")
+def list_dict():
+    stmt = db.session.query(data_base_table).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    list_dict = []
+    for index, row in df.iterrows():
+        x = {"Index": row['level_0'],
+        "Stock Name": row['Stock Name'],
+        "Reported Date": row['Reported Date'],
+        "Earnings Per Share": row['Earnings Per Share'],
+        "Forecasted Earnings Per Share": row['Forecasted Earnings Per Share'],
+        "% Surprise": row['% Surprise'],}
+        list_dict.append(x)
+
+    return(jsonify(list_dict))
+
+if __name__ == "__main__":
+    app.run()
