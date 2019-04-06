3786d6e6a664a042a199967823fbd96239b21d4a
Daniel Kim <kim866@gmail.com>
2019-04-06 13:40:07 -0400

    update

---
+++ logic draft.js
@@ -0,0 +1,52 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+markers = [];
+
+var greenIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 40],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+
+d3.csv("DowJonesPlus3_Coordinations.csv", function(error, csvData) {
+  if (error) console.log(error);
+
+  console.log(csvData);
+
+  csvData.forEach(function(data){
+    console.log(data)
+    var marker = new L.Marker([+data.Latitude, +data.Longitude], {icon: greenIcon});
+    marker.desc = data.Name;
+    myMap.addLayer(marker);
+    oms.addMarker(marker);
+
+  });
+
+
+});
+
+var oms = new OverlappingMarkerSpiderfier(myMap, {keepSpiderfied: true});
+
+var popup = new L.Popup();
+
+oms.addListener('click', function(marker) {
+  popup.setContent(marker.desc);
+  popup.setLatLng(marker.getLatLng());
+  myMap.openPopup(popup);
+});
+
--- logic.js
+++ logic.js
@@ -40,9 +40,18 @@ d3.csv("DowJonesPlus3_Coordinations.csv", function(error, csvData) {
 
   headquarters.addTo(myMap);
 */
+  var greenIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 40],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
   csvData.forEach(function(data){
     console.log(data)
-    var marker = new L.Marker([+data.Latitude, +data.Longitude]);
+    var marker = new L.Marker([+data.Latitude, +data.Longitude], {icon: greenIcon});
     marker.desc = data.Name;
     myMap.addLayer(marker);
     oms.addMarker(marker);
